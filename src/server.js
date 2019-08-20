7ffa3bcc7fb326db9e03e24596544542930b0513
Luccas Galuppo Tanan <luccasg.tanan@gmail.com>
2019-08-20 10:31:52 -0300

    Adicao de dependencia

--- src/server.js
+++ src/server.js
@@ -1,13 +1,14 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const routes = require('./routes');
-
-const server = express();
+const express = require('express')
+const mongoose = require('mongoose')
+const cors = require('cors')
+const routes = require('./routes')
+const server = express()
 
 mongoose.connect('mongodb+srv://admin:admin123@cluster0-cealv.mongodb.net/omnistack?retryWrites=true&w=majority', {
     useNewUrlParser : true
 })
 
+server.use(cors())
 server.use(express.json())
-server.use(routes); //colocar outras configuracoes que estão em outros ambientes
-server.listen(3333);  
+server.use(routes) //colocar outras configuracoes que estão em outros ambientes
+server.listen(3333); 
